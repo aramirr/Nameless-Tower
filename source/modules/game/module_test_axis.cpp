4d00b07494d6d56b540ac866ab45592e51ec3f6e
Juan Abadia <yabadabu@gmail.com>
2018-02-08 23:36:03 +0100

    flyover component for the camera

--- source/modules/game/module_test_axis.cpp
+++ source/modules/game/module_test_axis.cpp
@@ -14,75 +14,7 @@
 #include "components/comp_camera.h"
 #include "entity/entity_parser.h"
 
-/*
-struct TControllerFPS {
-  
-  VEC2    cursor;
-  VEC3    speed;
-  float   rotation_sensibility = deg2rad(65.f) / 150.0f;
-  float   pan_sensibility = 0.7f;
-  float   speed_reduction_factor = 0.95f;
-
-  void update(CCamera& camera, float dt ) {
-    
-    VEC3 origin = camera.getPosition();
-
-    speed *= speed_reduction_factor;
-
-    float dyaw = 0.f, dpitch = 0.f;
-    
-    VEC3 front = camera.getFront();
-    float yaw, pitch;
-    getYawPitchFromVector(front, &yaw, &pitch);
-
-    VEC2 prev_cursor = cursor;
-    cursor = VEC2(ImGui::GetMousePos().x, ImGui::GetMousePos().y );
-    VEC2 delta_cursor = cursor - prev_cursor;
-
-    if (ImGui::IsMouseDown(1)) {
-      if (delta_cursor.x || delta_cursor.y) {
-        dyaw -= delta_cursor.x;
-        dpitch -= delta_cursor.y;
-      }
-
-      if (isPressed('W'))
-        speed.z += 1.f;
-      if (isPressed('S'))
-        speed.z -= 1.f;
-      if (isPressed('A'))
-        speed.x += 1.f;
-      if (isPressed('D'))
-        speed.x -= 1.f;
-    }
-
-    yaw = yaw + dyaw * rotation_sensibility;
-    pitch = pitch + dpitch * rotation_sensibility;
-    const float max_pitch = deg2rad(90.f - 1e-2f);
-    if (pitch > max_pitch)
-      pitch = max_pitch;
-    else if (pitch < -max_pitch)
-      pitch = -max_pitch;
-
-    origin += camera.getLeft() * pan_sensibility * speed.x * dt;
-    origin += camera.getFront() * pan_sensibility * speed.z * dt;
-    origin += camera.getUp() * pan_sensibility * speed.y * dt;
-
-    VEC3 new_front = getVectorFromYawPitch(yaw, pitch);
-    VEC3 target = origin + new_front;
-    camera.lookAt(origin, target, VEC3(0, 1, 0));
-  }
-
-  void debugInMenu() {
-    ImGui::DragFloat("Rotation Sensibility", &rotation_sensibility, 0.001f, 0.001f, 0.01f);
-    ImGui::DragFloat("Pan Sensibility", &pan_sensibility, 0.01f, 0.1f, 5.f);
-    ImGui::DragFloat("Inertia", &speed_reduction_factor, 0.001f, 0.7f, 1.f);
-  }
-
-};
-*/
-
 CCamera        camera;
-//TControllerFPS controller_fps;
 
 bool CModuleTestAxis::start()
 {
@@ -121,8 +53,6 @@ bool CModuleTestAxis::stop()
 
 void CModuleTestAxis::update(float delta)
 {
-  //controller_fps.update(camera, delta);
-  //controller_fps.debugInMenu();
 }
 
 void CModuleTestAxis::render()
