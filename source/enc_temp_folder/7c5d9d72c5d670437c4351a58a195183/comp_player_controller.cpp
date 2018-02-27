4e66c4e8849d3b682d8676efdae4efb3b5b41bf0
Leandro Burgos <lburgosabal@gmail.com>
2018-02-27 20:44:10 +0100

    Acorto el omnidash

--- source/enc_temp_folder/7c5d9d72c5d670437c4351a58a195183/comp_player_controller.cpp
+++ source/enc_temp_folder/7c5d9d72c5d670437c4351a58a195183/comp_player_controller.cpp
@@ -110,7 +110,7 @@ void TCompPlayerController::load(const json& j, TEntityParseContext& ctx) {
 	tower_radius = j.value("tower_radius", 32.f);
 	dashing_speed = j.value("dashing_speed", 3);
 	omnidash_max_time = j.value("omnidash_max_time", 0.3);
-	omnidashing_max_ammount = j.value("omnidashing_max_ammount", 1.6f);
+	omnidashing_max_ammount = j.value("omnidashing_max_ammount", 2);
 	current_x_speed_factor = x_speed_factor;
 	is_grounded = true;
 	can_omni = true;
