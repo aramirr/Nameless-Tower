dbc75ccc86707d2b68ce9e06fb2371c7b1dbfa73
Jordi Nieves <jordinieves1985@gmail.com>
2018-03-04 14:53:09 +0100

    Respawnable Plattforms

--- source/components/ia/ai_destoyable.cpp
+++ source/components/ia/ai_destoyable.cpp
@@ -7,79 +7,136 @@
 
 DECL_OBJ_MANAGER("ai_destroyable", CAIDestroyable);
 
+using namespace physx;
+
 void CAIDestroyable::Init()
 {
-  // insert all states in the map
-  AddState("idle", (statehandler)&CAIDestroyable::IdleState);
-  AddState("trigger_destroy", (statehandler)&CAIDestroyable::TriggerDestroyState);
-  AddState("destroy", (statehandler)&CAIDestroyable::DestoyState);
+	// insert all states in the map
+	AddState("idle", (statehandler)&CAIDestroyable::IdleState);
+	AddState("trigger_destroy", (statehandler)&CAIDestroyable::TriggerDestroyState);
+	AddState("transition_destroy", (statehandler)&CAIDestroyable::TransitionDestroyState);
+	AddState("destroy", (statehandler)&CAIDestroyable::DestoyState);
 
-  // reset the state
-  ChangeState("idle");
+
+	// reset the state
+	ChangeState("idle");
 }
 
 void CAIDestroyable::debugInMenu() {
 
-  IAIController::debugInMenu();
-  CEntity *c_trigger = (CEntity *)getEntityByName(trigger_actor);
-  if (!c_trigger)
-	  return;
+	IAIController::debugInMenu();
+	CEntity *c_trigger = (CEntity *)getEntityByName(trigger_actor);
+	if (!c_trigger)
+		return;
 
-  TCompTransform *mypos = getMyTransform();
-  TCompTransform *ppos = c_trigger->get<TCompTransform>();
-     
-      ImGui::Text("Distance %f", VEC3::Distance(mypos->getPosition(), ppos->getPosition()));
-	  ImGui::Text("acum_time %f", acum_time);
-      
-  
+	TCompTransform *mypos = getMyTransform();
+	TCompTransform *ppos = c_trigger->get<TCompTransform>();
+
+	ImGui::Text("Distance %f", VEC3::Distance(mypos->getPosition(), ppos->getPosition()));
+	ImGui::Text("acum_time %f", acum_time);
+
+
 }
 
 void CAIDestroyable::load(const json& j, TEntityParseContext& ctx) {
-  setEntity(ctx.current_entity);
+	setEntity(ctx.current_entity);
 
-  Init();
+	Init();
 
-  trigger_actor = j.value("trigger_actor", "");
-  destroy_time = j.value("destroy_time", 1.0f);
-  acum_time = 0;
+	trigger_actor = j.value("trigger_actor", "");
+	destroy_time = j.value("destroy_time", 1.0f);
+	recover_time = j.value("recover_time", 5.0f);
+	acum_time = 0;
 }
 
 
 
 void CAIDestroyable::IdleState()
 {
-  change_color(VEC4(0, 1, 0, 1));
-  CEntity *c_trigger = (CEntity *)getEntityByName(trigger_actor);
-  if (!c_trigger)
-	  return;
+	change_color(VEC4(0, 1, 0, 1));
+	CEntity *c_trigger = (CEntity *)getEntityByName(trigger_actor);
+	if (!c_trigger)
+		return;
 
-  TCompTransform *mypos = getMyTransform();
-  TCompTransform *ppos = c_trigger->get<TCompTransform>();
+	TCompTransform *mypos = getMyTransform();
+	TCompTransform *ppos = c_trigger->get<TCompTransform>();
 
-  float distance = VEC3::Distance(mypos->getPosition(), ppos->getPosition());
-  if (distance < 0.5f) {
-	  acum_time = 0.0f;
-	  ChangeState("trigger_destroy");
-  }
+	float distance = VEC3::Distance(mypos->getPosition(), ppos->getPosition());
+	if (distance < 0.75f) {
+		acum_time = 0.0f;
+		ChangeState("trigger_destroy");
+	}
 }
 
 
 void CAIDestroyable::TriggerDestroyState(float dt)
 {
-	change_color(VEC4 (1, 0, 0, 1));
+	change_color(VEC4(1, 0, 0, 1));
 	acum_time += dt;
 	if (acum_time >= destroy_time)
-		ChangeState("destroy");
+	{
+		acum_time = 0;
+		ChangeState("transition_destroy");
+	}
 }
 
-void CAIDestroyable::DestoyState()
+void CAIDestroyable::TransitionDestroyState(float dt)
 {
+	change_color(VEC4(1, 1, 0, 1));
+	TCompTransform *mypos = getMyTransform();
+	if (!mypos) {
+		return;
+	}
+	current_pos = mypos->getPosition();
+	mypos->setPosition(VEC3(0, 0, 0));
 	TCompCollider* comp_collider = get<TCompCollider>();
 	if (comp_collider)
 	{
-		comp_collider->actor->release();
+		PxRigidActor* rigidActor = ((PxRigidActor*)comp_collider->actor);
+		PxTransform tr = rigidActor->getGlobalPose();
+		tr.p = PxVec3(0, 0, 0);
+		rigidActor->setGlobalPose(tr);
 	}
-	CHandle(this).getOwner().destroy();
+	ChangeState("destroy");
+}
+
+void CAIDestroyable::DestoyState(float dt)
+{
+	change_color(VEC4(1, 0, 1, 1));
+	acum_time += dt;
+	if (acum_time >= recover_time)
+	{
+		acum_time = 0;
+		TCompTransform *mypos = getMyTransform();
+		if (!mypos) {
+			return;
+		}
+
+		CEntity *p_e = (CEntity *)getEntityByName(trigger_actor);
+		if (!p_e)
+			return;
+
+		TCompTransform *t_ppos = p_e->get<TCompTransform>();
+		if (!t_ppos) {
+			return;
+		}
+
+		float distance = VEC3::Distance(current_pos, t_ppos->getPosition());
+		if (distance < 1.5f) {
+			return;
+		}
+
+		mypos->setPosition(current_pos);
+		TCompCollider* comp_collider = get<TCompCollider>();
+		if (comp_collider)
+		{
+			PxRigidActor* rigidActor = ((PxRigidActor*)comp_collider->actor);
+			PxTransform tr = rigidActor->getGlobalPose();
+			tr.p = PxVec3(current_pos.x, current_pos.y, current_pos.z);
+			rigidActor->setGlobalPose(tr);
+		}
+		ChangeState("idle");
+	}
 }
 
 void CAIDestroyable::change_color(VEC4 color) {
