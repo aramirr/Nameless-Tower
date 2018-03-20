20086a67d37fe81fbd3783247c3a6000c16ec3fc
Juan <john@novarama.com>
2018-03-20 21:12:04 +0100

    texture.create from params, not from a file

--- source/render/texture/texture.h
+++ source/render/texture/texture.h
@@ -5,9 +5,12 @@
 // ----------------------------------------------
 class CTexture : public IResource {
   
+protected:
   // DX
   ID3D11Resource*           texture = nullptr;
   ID3D11ShaderResourceView* shader_resource_view = nullptr; 
+  int                       xres = 0;
+  int                       yres = 0;
 
 public:
   void activate(int slot) const;
@@ -17,9 +20,23 @@ public:
   void onFileChanged(const std::string& filename) override;
   static void setNullTexture(int slot);
 
+  int  getWidth() const { return xres; }
+  int  getHeight() const { return yres; }
+
   // DX stuff for the multimaterial
   const ID3D11ShaderResourceView* getShaderResourceView() const {
     return shader_resource_view;
   }
+
+  // Create a new texture from params
+  enum TCreateOptions {
+    CREATE_STATIC
+  , CREATE_DYNAMIC
+  , CREATE_RENDER_TARGET
+  };
+  bool create(int new_xres, int new_yres, DXGI_FORMAT new_color_format, TCreateOptions create_options = CREATE_STATIC);
+  void setDXParams(int new_xres, int new_yres, ID3D11Texture2D* new_texture, ID3D11ShaderResourceView* new_srv);
+
+
 };
 
