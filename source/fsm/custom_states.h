f9090b564d42615788f3b522bda817c964ea5e51
Leandro Burgos <lburgosabal@gmail.com>
2018-03-22 20:58:52 +0100

    Borro includes

--- source/fsm/custom_states.h
+++ source/fsm/custom_states.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include "state.h"
-#include "components/juan/comp_transform.h"
-#include "components/camera/comp_camera.h"
-#include "components/ui/ui_mouse_pos.h"
+
 
 namespace FSM
 {
