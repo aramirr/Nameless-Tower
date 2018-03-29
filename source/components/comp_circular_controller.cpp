ab1d4388fac0e17e3a457210bbcb93f7845c3755
ManuJGQ <manueljgq@gmail.com>
2018-03-29 17:02:05 +0200

    Componente giro sobre su propio eje (Falta averiguar el bug del eje Z)

--- source/components/comp_circular_controller.cpp
+++ source/components/comp_circular_controller.cpp
@@ -22,24 +22,35 @@ void TCompCircularController::update(float dt) {
 
   TCompTransform *c_my_transform = get<TCompTransform>();
   
-  float X = 0.f;
-  float Y = 0.f;
-  float Z = 0.f;
-  VEC3 up = c_my_transform->getRotation();
+  //float X = 0.f;
+  //float Y = 0.f;
+  //float Z = 0.f;
+  QUAT rot = c_my_transform->getRotation();
+  VEC3 front = c_my_transform->getFront();
+  VEC3 pos = c_my_transform->getPosition();
 
-  if (up.x > up.y && up.x > up.z)X += 1.f;
-  if (up.y > up.x && up.y > up.z)Y += 1.f;
-  if (up.z > up.x && up.z > up.y)Z += 1.f;
+  //if (up.x > up.y && up.x > up.z)X += 1.f;
+  //if (up.y > up.x && up.y > up.z)Y += 1.f;
+  //if (up.z > up.x && up.z > up.y)Z += 1.f;
 
   float y, p, r;
   c_my_transform->getYawPitchRoll(&y, &p, &r);
-  c_my_transform->setYawPitchRoll(curr_yaw * up.y * Y, curr_yaw * up.z * Z, curr_yaw * up.x * X);
 
-  std::string str = std::to_string(up.x) + " - " + std::to_string(up.y) + " - " + std::to_string(up.z);
+  c_my_transform->setPosition(VEC3(0, 0, 0));
 
-  dbg("------------------------------------------------------------------------------\n");
-  dbg(str.c_str());
-  dbg("\n");
+  //c_my_transform->setRotation(QUAT(rot.x, rot.y, rot.z, rot.w * -1));
+
+  c_my_transform->setYawPitchRoll(y /** up.y * Y*/, curr_yaw /*curr_yaw * up.z * Z*/, r/*curr_yaw * up.x * X*/);
+
+  //c_my_transform->setRotation(QUAT(rot.x, rot.y, rot.z, rot.w));
+
+  c_my_transform->setPosition(pos);
+
+  //std::string str = std::to_string(pos.x) + " - " + std::to_string(pos.y) + " - " + std::to_string(pos.z);
+
+  //dbg("------------------------------------------------------------------------------\n");
+  //dbg(str.c_str());
+  //dbg("\n");
 
   assert(c_my_transform);
 }
