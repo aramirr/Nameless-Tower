969632c89afa9252c4ae13a90c9b404df8105cb3
mcv16_alumne <mcv16_alumne@A209W10PC01>
2018-04-05 18:48:15 +0200

    Arreglado bug en la velocidad de giro

--- source/components/comp_circular_controller.cpp
+++ source/components/comp_circular_controller.cpp
@@ -11,27 +11,27 @@ void TCompCircularController::debugInMenu() {
 
 
 void TCompCircularController::load(const json& j, TEntityParseContext& ctx) {
-	speed = j.value("speed", 1.0f) * 0.00001f;
-  eje = *((j.value("eje", "X")).c_str());
+	speed = j.value("speed", 1.0f) * 0.0001f;
+	eje = *((j.value("eje", "X")).c_str());
 
 	carga = true;
 }
 
 void TCompCircularController::update(float dt) {
 
-	curr_rad += speed * dt;
+	//curr_rad += speed * dt;
 
-  float DEG = rad2deg(curr_rad);
-  float COS = cos(DEG);
-  float SIN = sin(DEG);
+	float DEG = rad2deg(speed);
+	float COS = cos(DEG);
+	float SIN = sin(DEG);
 
 	TCompTransform *c_my_transform = get<TCompTransform>();
 
 	if (carga) {    //CARGA INICIAL
 
-		if(eje == 'X')u = c_my_transform->getFront();
-    else if (eje == 'Y')u = c_my_transform->getUp();
-    else if (eje == 'Z')u = c_my_transform->getLeft();
+		if (eje == 'X')u = c_my_transform->getFront();
+		else if (eje == 'Y')u = c_my_transform->getUp();
+		else if (eje == 'Z')u = c_my_transform->getLeft();
 		u.Normalize();
 
 		carga = false;
@@ -40,12 +40,18 @@ void TCompCircularController::update(float dt) {
 	MAT44 posM = c_my_transform->asMatrix();
 
 	posM *= MAT44(COS + pow(u.x, 2) * (1 - COS), u.x * u.y * (1 - COS) - u.z * SIN, u.x * u.z * (1 - COS) + u.y * SIN, 0,
-				  u.y * u.x * (1 - COS) + u.z * SIN, COS + pow(u.y, 2) * (1 - COS), u.y * u.z * (1 - COS) - u.x * SIN, 0,
-				  u.z * u.x * (1 - COS) - u.y * SIN, u.z * u.y * (1 - COS) + u.x * SIN, COS + pow(u.z, 2) * (1 - COS), 0,
-				  0, 0, 0, 1);
+		u.y * u.x * (1 - COS) + u.z * SIN, COS + pow(u.y, 2) * (1 - COS), u.y * u.z * (1 - COS) - u.x * SIN, 0,
+		u.z * u.x * (1 - COS) - u.y * SIN, u.z * u.y * (1 - COS) + u.x * SIN, COS + pow(u.z, 2) * (1 - COS), 0,
+		0, 0, 0, 1);
 
 	c_my_transform->setRotation(QUAT::CreateFromRotationMatrix(posM));
 
 	assert(c_my_transform);
+
+	//std::string str = std::to_string(DEG);
+
+	//dbg("------------------------------------------------------------------------------\n");
+	//dbg(str.c_str());
+	//dbg("\n");
 }
 
