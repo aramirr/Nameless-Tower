ef99b0ac0b91751a43b2fa358f723d4308e0ceb1
Juan Abadia <john@novarama.com>
2018-04-10 15:04:16 +0200

    Disable keyboard if the app does NOT have the focus

--- source/windows/app.h
+++ source/windows/app.h
@@ -16,6 +16,7 @@ class CApp {
   int              yres = 0;
   CTimer           time_since_last_render;
   CDirectoyWatcher resources_dir_watcher;
+  bool             has_focus = false;
 
   static CApp* app_instance;
 
@@ -38,6 +39,8 @@ public:
     assert(app_instance == this);
     app_instance = nullptr;
   }
+
+  bool hasFocus() const { return has_focus; }
 
   // Remove copy ctor
   CApp(const CApp&) = delete;
