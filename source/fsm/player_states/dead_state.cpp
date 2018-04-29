0ea91d9d5e91241e56eb619c80db314c4d63131c
Leandro Burgos <lburgosabal@gmail.com>
2018-04-29 06:58:34 -0700

    Agrego animaciones de muerte y planear

--- source/fsm/player_states/dead_state.cpp
+++ source/fsm/player_states/dead_state.cpp
@@ -11,7 +11,7 @@ namespace FSM
 	{
 		CEntity* e = ctx.getOwner();
 		TCompPlayerController* player = e->get<TCompPlayerController>();
-		//player->change_mesh(player->EAnimations::EDead);	
+		player->change_animation(player->EAnimations::NajaDeath, _is_action, _delay_in, _delay_out);
 		ctx.setVariable("hit", false);
 	}
 
