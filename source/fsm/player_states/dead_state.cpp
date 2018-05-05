1fa3a1b1309d5ef126488dba778606518c65a8d6
Leandro Burgos <lburgosabal@gmail.com>
2018-05-05 12:11:53 -0700

    Reinicio altura de salto para muerte en dash y omnidash

--- source/fsm/player_states/dead_state.cpp
+++ source/fsm/player_states/dead_state.cpp
@@ -13,6 +13,8 @@ namespace FSM
 		CEntity* e = ctx.getOwner();
 		TCompPlayerController* player = e->get<TCompPlayerController>();
 		player->change_animation(player->EAnimations::NajaDead, _is_action, _delay_in, _delay_out);
+		TCompTransform *c_my_transform = e->get<TCompTransform>();
+		player->jumping_start_height = c_my_transform->getPosition().y;
 		ctx.setVariable("hit", false);
 	}
 
