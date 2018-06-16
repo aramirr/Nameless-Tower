55a8949f4e15c87593b8517ff33bc545a1922203
Manuel Jesus Garcia Quesada <manueljgq@gmail.com>
2018-06-16 12:09:47 +0200

    Agregada luz spot

--- Bin/data/shaders/ctes.h
+++ Bin/data/shaders/ctes.h
@@ -96,125 +96,134 @@
 
 CB_DEF(CCteCamera, CB_CAMERA)   // Generates the b0
 {
-  MAT44 camera_view;
-  MAT44 camera_proj;
-  MAT44 camera_view_proj;
-  MAT44 camera_screen_to_world;
-  MAT44 camera_proj_with_offset;      // Used by ao
-  VEC3  camera_pos;
-  float camera_dummy1;
-  VEC3  camera_front;
-  float camera_dummy2;
-  VEC3  camera_left;
-  float camera_dummy3;
-  VEC3  camera_up;
-  float camera_dummy4;
+	MAT44 camera_view;
+	MAT44 camera_proj;
+	MAT44 camera_view_proj;
+	MAT44 camera_screen_to_world;
+	MAT44 camera_proj_with_offset;      // Used by ao
+	VEC3  camera_pos;
+	float camera_dummy1;
+	VEC3  camera_front;
+	float camera_dummy2;
+	VEC3  camera_left;
+	float camera_dummy3;
+	VEC3  camera_up;
+	float camera_dummy4;
 
-  float camera_zfar;
-  float camera_znear;
-  float camera_tan_half_fov;
-  float camera_aspect_ratio;
+	float camera_zfar;
+	float camera_znear;
+	float camera_tan_half_fov;
+	float camera_aspect_ratio;
 
-  VEC2  camera_inv_resolution;
-  float camera_dummy5;
-  float camera_dummy6;
+	VEC2  camera_inv_resolution;
+	float camera_dummy5;
+	float camera_dummy6;
 };
 
 CB_DEF(CCteObject, CB_OBJECT)   // Generates the b1
 {
-  MAT44 obj_world;
-  VEC4  obj_color;
+	MAT44 obj_world;
+	VEC4  obj_color;
 };
 
 CB_DEF(CCteSkinBones, CB_SKIN_BONES)
 {
-  MAT44 Bones[MAX_SUPPORTED_BONES];
+	MAT44 Bones[MAX_SUPPORTED_BONES];
 };
 
 CB_DEF(CCteLight, CB_LIGHT)
 {
-  VEC4  light_color;
-  VEC3  light_pos;
-  float light_intensity;
-  // Align x4
-  float light_shadows_step;
-  float light_shadows_inverse_resolution;     // 1.0f / 256.0
-  float light_shadows_step_with_inv_res;      // light_shadows_step * light_shadows_inverse_resolution
-  float light_radius;                         // Used by point lights
-  // Align x4
-  MAT44 light_view_proj_offset;
+	VEC4  light_color;
+	VEC3  light_pos;
+	float light_intensity;
+	VEC4  light_direction;
+
+	// Align x4
+	float light_shadows_step;
+	float light_shadows_inverse_resolution;     // 1.0f / 256.0
+	float light_shadows_step_with_inv_res;      // light_shadows_step * light_shadows_inverse_resolution
+	float light_radius;                         // Used by point lights
+
+	//Some spotlight related values.
+	float light_angle;
+	float light_inner_cut;
+	float light_outer_cut;
+	float dummy2;
+
+	// Align x4
+	MAT44 light_view_proj_offset;
 
 };
 
 CB_DEF(CCteGlobals, CB_GLOBALS)
 {
-  float global_world_time;
-  float global_exposure_adjustment;
-  float global_ambient_adjustment;
-  float global_hdr_enabled;
+	float global_world_time;
+	float global_exposure_adjustment;
+	float global_ambient_adjustment;
+	float global_hdr_enabled;
 
-  float global_gamma_correction_enabled;
-  float global_tone_mapping_mode;
-  int   global_render_output;
-  float global_dummy;
+	float global_gamma_correction_enabled;
+	float global_tone_mapping_mode;
+	int   global_render_output;
+	float global_dummy;
 
-  float global_shared_fx_amount;
-  float global_shared_fx_val1;
-  float global_shared_fx_val2;
-  float global_shared_fx_val3;
+	float global_shared_fx_amount;
+	float global_shared_fx_val1;
+	float global_shared_fx_val2;
+	float global_shared_fx_val3;
 
 };
 
-CB_DEF(CCteMaterial, CB_MATERIAL) 
+CB_DEF(CCteMaterial, CB_MATERIAL)
 {
-  float  scalar_roughness;
-  float  scalar_metallic;
-  float  scalar_irradiance_vs_mipmaps;
-  float  material_dummy;
+	float  scalar_roughness;
+	float  scalar_metallic;
+	float  scalar_irradiance_vs_mipmaps;
+	float  material_dummy;
 
-  float  mix_boost_r;
-  float  mix_boost_g;
-  float  mix_boost_b;
-  float  material_dummy2;
+	float  mix_boost_r;
+	float  mix_boost_g;
+	float  mix_boost_b;
+	float  material_dummy2;
 };
 
 CB_DEF(CCteBlur, CB_BLUR)
 {
-  VEC4 blur_w;        // weights
-  VEC4 blur_d;        // distances for the 1st, 2nd and 3rd tap
-  VEC2 blur_step;     // Extra modifier
-  VEC2 blur_center; // To keep aligned x4
+	VEC4 blur_w;        // weights
+	VEC4 blur_d;        // distances for the 1st, 2nd and 3rd tap
+	VEC2 blur_step;     // Extra modifier
+	VEC2 blur_center; // To keep aligned x4
 };
 
 CB_DEF(CCteBloom, CB_BLOOM)
 {
-  VEC4  bloom_weights;
-  float bloom_threshold_min;
-  float bloom_threshold_max;
-  float bloom_pad1;
-  float bloom_pad2;
+	VEC4  bloom_weights;
+	float bloom_threshold_min;
+	float bloom_threshold_max;
+	float bloom_pad1;
+	float bloom_pad2;
 };
 
 CB_DEF(CCteFocus, CB_FOCUS)
 {
-  float focus_z_center_in_focus;
-  float focus_z_margin_in_focus;
-  float focus_transition_distance;
-  float focus_modifier;
+	float focus_z_center_in_focus;
+	float focus_z_margin_in_focus;
+	float focus_transition_distance;
+	float focus_modifier;
 };
 
 CB_DEF(CCteGUI, CB_GUI)
 {
-  VEC2 minUV;
-  VEC2 maxUV;
-  VEC4 tint_color;
+	VEC2 minUV;
+	VEC2 maxUV;
+	VEC4 tint_color;
 };
 
 CB_DEF(CCteParticle, CB_PARTICLE)
 {
-  VEC2 particle_minUV;
-  VEC2 particle_maxUV;
-  VEC4 particle_color;
+	VEC2 particle_minUV;
+	VEC2 particle_maxUV;
+	VEC4 particle_color;
 };
 
 
--- source/components/comp_light_spot.cpp
+++ source/components/comp_light_spot.cpp
@@ -1,5 +1,5 @@
 #include "mcv_platform.h"
-#include "comp_light_dir.h"
+#include "comp_light_spot.h"
 #include "components/juan/comp_transform.h"
 #include "render/render_objects.h"    // cb_light
 #include "render/texture/texture.h" 
@@ -7,29 +7,41 @@
 #include "render/texture/render_to_texture.h" 
 #include "render/render_manager.h" 
 
-DECL_OBJ_MANAGER("light_dir", TCompLightDir);
+DECL_OBJ_MANAGER("light_spot", TCompLightSpot);
 
-DXGI_FORMAT readFormat(const json& j, const std::string& label);
+//DXGI_FORMAT readFormat(const json& j, const std::string& label);
 
 // -------------------------------------------------
-void TCompLightDir::debugInMenu() {
+void TCompLightSpot::debugInMenu() {
 	TCompCamera::debugInMenu();
 	ImGui::DragFloat("Intensity", &intensity, 0.01f, 0.f, 10.f);
 	ImGui::ColorEdit3("Color", &color.x);
 	ImGui::DragFloat("Step Size", &shadows_step, 0.01f, 0.f, 5.f);
+	ImGui::DragFloat("Angle", &angle, 0.5f, 1.f, 160.f);
+	ImGui::DragFloat("Cut Out", &inner_cut, 0.5f, 1.f, angle);
+	ImGui::DragFloat("Range", &range, 0.5f, 1.f, 120.f);
 }
 
 // -------------------------------------------------
-void TCompLightDir::renderDebug() {
+void TCompLightSpot::renderDebug() {
   TCompCamera::renderDebug();
+
+	// Render a wire sphere
+	//auto mesh = Resources.get("data/meshes/UnitCone.mesh")->as<CRenderMesh>();
+	//renderMesh(mesh, getWorld(), VEC4(1, 1, 1, 1));
 }
 
 // -------------------------------------------------
-void TCompLightDir::load(const json& j, TEntityParseContext& ctx) {
+void TCompLightSpot::load(const json& j, TEntityParseContext& ctx) {
   TCompCamera::load( j, ctx );
   if(j.count("color"))
     color = loadVEC4(j["color"]);
   intensity = j.value("intensity", intensity);
+
+	angle = j.value("angle", 45.f);
+	range = j.value("range", 10.f);
+	inner_cut = j.value("inner_cut", angle);
+	outer_cut = j.value("outer_cut", angle);
 
   if( j.count("projector")) {
     std::string projector_name = j.value("projector", "");
@@ -59,7 +71,22 @@ void TCompLightDir::load(const json& j, TEntityParseContext& ctx) {
   shadows_enabled = casts_shadows;
 }
 
-void TCompLightDir::update(float dt) {
+void TCompLightSpot::setColor(const VEC4 & new_color) {
+
+	color = new_color;
+}
+
+MAT44 TCompLightSpot::getWorld() {
+
+	TCompTransform* c = get<TCompTransform>();
+	if (!c)
+		return MAT44::Identity;
+
+	float new_scale = tan(deg2rad(angle * .5f)) * range;
+	return MAT44::CreateScale(VEC3(new_scale, new_scale, range)) * c->asMatrix();
+}
+
+void TCompLightSpot::update(float dt) {
   // Can't use the TCompCamera::update because inside it calls 
   // get<TCompTransform> which tries to convert 'this' to an instance
   // of TCompCamera, but will fail because we are a CompLightDir
@@ -67,11 +94,12 @@ void TCompLightDir::update(float dt) {
   if (!c)
     return;
   this->lookAt(c->getPosition(), c->getPosition() + c->getFront(), c->getUp());
+	this->setPerspective(deg2rad(angle), 0.1f, range); // might change this znear in the future, hardcoded for clipping purposes.
 }
 
 // -------------------------------------------------
 // Updates the Shader Cte Light with MY information
-void TCompLightDir::activate() {
+void TCompLightSpot::activate() {
   TCompTransform* c = get<TCompTransform>();
   if (!c)
     return;
@@ -84,12 +112,16 @@ void TCompLightDir::activate() {
   MAT44 mtx_offset = MAT44::CreateScale(VEC3(0.5f, -0.5f, 1.0f))
                    * MAT44::CreateTranslation(VEC3(0.5f, 0.5f, 0.0f));
 
-  cb_light.light_color = color;
-  cb_light.light_intensity = intensity;
-  cb_light.light_pos = c->getPosition();
-  cb_light.light_radius = getZFar();
-  cb_light.light_view_proj_offset = getViewProjection() * mtx_offset;
-	cb_light.light_angle = 0;
+	float spot_angle = cos(deg2rad(angle * .5f));
+	cb_light.light_color = color;
+	cb_light.light_intensity = intensity;
+	cb_light.light_pos = c->getPosition();
+	cb_light.light_radius = range * c->getScale();
+	cb_light.light_view_proj_offset = getViewProjection() * mtx_offset;
+	cb_light.light_angle = spot_angle;
+	cb_light.light_direction = VEC4(c->getFront().x, c->getFront().y, c->getFront().z, 1);
+	cb_light.light_inner_cut = cos(deg2rad(clamp(inner_cut, 0.f, angle) * .5f));
+	cb_light.light_outer_cut = spot_angle;
   cb_light.updateGPU();
 
   // If we have a ZTexture, it's the time to activate it
@@ -98,7 +130,7 @@ void TCompLightDir::activate() {
     cb_light.light_shadows_inverse_resolution = 1.0f / (float)shadows_rt->getWidth();
     cb_light.light_shadows_step = shadows_step;
     cb_light.light_shadows_step_with_inv_res = shadows_step / (float)shadows_rt->getWidth();
-    //cb_light.light_radius = 1.f;
+    cb_light.light_radius = range;
 
     assert(shadows_rt->getZTexture());
     shadows_rt->getZTexture()->activate(TS_LIGHT_SHADOW_MAP);
@@ -109,7 +141,7 @@ void TCompLightDir::activate() {
 
 
 // ------------------------------------------------------
-void TCompLightDir::generateShadowMap() {
+void TCompLightSpot::generateShadowMap() {
   if (!shadows_rt || !shadows_enabled )
     return;
 
