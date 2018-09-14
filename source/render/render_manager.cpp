0760cd0e655fc07345e3fed78d92bad8b0a1157d
aramirr <eduardcasellas18@gmail.com>
2018-09-14 19:21:14 +0200

    Añadida prioridad al material

--- source/render/render_manager.cpp
+++ source/render/render_manager.cpp
@@ -38,10 +38,10 @@ bool CRenderManager::sortRenderKeys(const TRenderKey& k1, const TRenderKey& k2) 
   // Render tech
 
   //JUAN DEBERES 
-  //auto p1 = k1.material->getPriority();
-  //auto p2 = k2.material->getPriority();
-  //if (p1 != p2)
-  //  return p1 < p2;
+  auto p1 = k1.material->getPriority();
+  auto p2 = k2.material->getPriority();
+  if (p1 != p2)
+    return p1 < p2;
 
   auto t1 = k1.material->tech;
   auto t2 = k2.material->tech;
