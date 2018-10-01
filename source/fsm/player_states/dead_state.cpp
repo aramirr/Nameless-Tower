4966971d43834ef1a099eab2162d14361a12daaa
Leandro Burgos <lburgosabal@gmail.com>
2018-10-01 12:02:58 +0200

    Pruebo arreglo para no morir doble

--- source/fsm/player_states/dead_state.cpp
+++ source/fsm/player_states/dead_state.cpp
@@ -12,7 +12,9 @@ namespace FSM
 	{
 		CEntity* e = ctx.getOwner();
 		TCompPlayerController* player = e->get<TCompPlayerController>();
-		player->change_animation(player->EAnimations::NajaDeath, _is_action, _delay_in, _delay_out, true);
+		if (player->can_die) {
+			player->change_animation(player->EAnimations::NajaDeath, _is_action, _delay_in, _delay_out, true);
+		}
 		ctx.setVariable("hit", false);
 	}
 
