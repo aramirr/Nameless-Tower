6c2033311fb0d8e3d68414f5c9cae692d361f60d
ManuJGQ <manueljgq@gmail.com>
2018-10-02 17:26:55 +0200

    Iniciado temblor camara

--- source/components/camera/comp_camera_manager.cpp
+++ source/components/camera/comp_camera_manager.cpp
@@ -80,6 +80,16 @@ void TCompCameraManager::activateCinematic(std::string name)
   }
 }
 
+void TCompCameraManager::activarTemblor()
+{
+	temblor = true;
+}
+
+void TCompCameraManager::desactivarTemblor()
+{
+	temblor = false;
+}
+
 void TCompCameraManager::debugInMenu() {
   //...
 }
@@ -97,36 +107,132 @@ void TCompCameraManager::load(const json& j, TEntityParseContext& ctx) {
 
   cinematics.clear();
 
+	temblor = true;
+
   cameraActive = 0;
   currentTime = 0.f;
   totalTime = 0.f;
 }
 
+//--------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------
+float dot(VEC2 a, VEC2 b) {
+	return a.x * b.x + a.y * b.y;
+}
+
+float random(VEC2 st)
+{
+	return (float)(sin(dot(st,VEC2(12.9898, 78.233)))* 43758.5453123) % 1;
+}
+
+//float noise(VEC2 st)
+//{
+//	VEC2 i = floor(st);
+//	VEC2 f = fract(st);
+//	float a = random(i);
+//	float b = random(i + VEC2(1.0, 0.0));
+//	float c = random(i + VEC2(0.0, 1.0));
+//	float d = random(i + VEC2(1.0, 1.0));
+//	VEC2 u = f * f * (3.0 - 2.0 * f);
+//	return mix(a, b, u.x) + (c - a)* u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+//}
+//
+//#define octaves 3
+//float fbm(VEC2 p)
+//{
+//	float value = 0.0;
+//	float freq = 1.13;
+//	float amp = 0.57;
+//	for (int i = 0; i < octaves; i++)
+//	{
+//		value += amp * (noise((p - VEC2(1.0)) * freq));
+//		freq *= 1.61;
+//		amp *= 0.47;
+//	}
+//	return value;
+//}
+//
+//float pat(VEC2 p)
+//{
+//	float time = iTime * 0.75;
+//	VEC2 aPos = VEC2(sin(time * 0.035), sin(time * 0.05)) * 3.;
+//	VEC2 aScale = VEC2(3.25);
+//	float a = fbm(p * aScale + aPos);
+//	VEC2 bPos = VEC2(sin(time * 0.09), sin(time * 0.11)) * 1.2;
+//	VEC2 bScale = VEC2(0.75);
+//	float b = fbm((p + a) * bScale + bPos);
+//	VEC2 cPos = VEC2(-0.6, -0.5) + VEC2(sin(-time * 0.01), sin(time * 0.1)) * 1.9;
+//	VEC2 cScale = VEC2(1.25);
+//	float c = fbm((p + b) * cScale + cPos);
+//	return c;
+//}
+
+VEC2 Shake(float maxshake, float mag, float dt)
+{
+	float speed = 20.0*mag;
+	float shakescale = maxshake * mag;
+
+	float time = dt * speed;			// speed of shake
+
+	VEC2 p1 = VEC2(0.25, 0.25);
+	VEC2 p2 = VEC2(0.75, 0.75);
+	p1 += VEC2(time);
+	p2 += VEC2(time);
+
+	// random shake is just too violent...
+	float val1 = random(p1);
+	float val2 = random(p2);
+
+	//float val1 = pat(p1);
+	//float val2 = pat(p2);
+	val1 = lerp(val1, 0.f, 1.f);
+	val2 = lerp(val2, 0.f, 1.f);
+
+	return VEC2(val1*shakescale, val2*shakescale);
+}
+
+//-----------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------
+
 void TCompCameraManager::update(float dt) {
 
-  if (isPressed(VK_F1)) {
-    CEntity* player = (CEntity*)getEntityByName("The Player");
+	if (temblor && isPressed(VK_F3)) {
+		CEntity* camera = (CEntity*)Engine.getCameras().getActiveCamera();
+		TCompTransform* c = camera->get<TCompTransform>();
+		float maxshake = 0.05;				// max shake amount
+		float mag = 0.5 + sin(dt)*0.5;		// shake magnitude...
 
-    TMsgSetFSMVariable pauseMsg;
-    pauseMsg.variant.setName("pause");
-    pauseMsg.variant.setBool(true);
+																				// *temp* , We will calc shakexy once in the vertex shader...
+		VEC2 shakexy = Shake(maxshake, mag, dt);
+		c->setPosition(c->getPosition() + shakexy);
+		c->lookAt(c->getPosition() + shakexy, VEC3(0, c->getPosition().y,0) + shakexy);
+	}
+	else {
+		if (isPressed(VK_F1)) {
+			CEntity* player = (CEntity*)getEntityByName("The Player");
 
-    player->sendMsg(pauseMsg);
+			TMsgSetFSMVariable pauseMsg;
+			pauseMsg.variant.setName("pause");
+			pauseMsg.variant.setBool(true);
 
-    godMode = true;
-  }
-  if (isPressed(VK_F2)) {
-    CEntity* player = (CEntity*)getEntityByName("The Player");
+			player->sendMsg(pauseMsg);
 
-    TMsgSetFSMVariable pauseMsg;
-    pauseMsg.variant.setName("idle");
-    pauseMsg.variant.setBool(true);
+			godMode = true;
+		}
+		if (isPressed(VK_F2)) {
+			CEntity* player = (CEntity*)getEntityByName("The Player");
 
-    player->sendMsg(pauseMsg);
+			TMsgSetFSMVariable pauseMsg;
+			pauseMsg.variant.setName("idle");
+			pauseMsg.variant.setBool(true);
 
-    godMode = false;
-  }
+			player->sendMsg(pauseMsg);
 
+			godMode = false;
+		}
+	}
 
   if (carga) {																				// CONFIGURACION INICIAL DEL MANAGER DE CAMARAS
     CHandle h_camera = getEntityByName("camera_orbit_IZQ");
